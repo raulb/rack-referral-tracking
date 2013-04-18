659e29c52c6b6abe7d14d7e4c4a02e28620f8c6d
Craig Kerstiens <craig.kerstiens@gmail.com>
2013-04-18 13:49:30 -0700

    fixing error in bad code

--- lib/rack-referral-tracking/middleware.rb
+++ lib/rack-referral-tracking/middleware.rb
@@ -17,7 +17,7 @@ module Rack
         cookies[:utm_campaign] = { :value => params[:utm_campaign], :expires => COOKIE_EXPIRES,:domain => uri.host } if params[:utm_campaign].present?
         cookies[:utm_source] = { :value => params[:utm_source], :expires => COOKIE_EXPIRES,:domain => uri.host } if params[:utm_source].present?
         cookies[:utm_medium] = { :value => params[:utm_medium], :expires => COOKIE_EXPIRES,:domain => uri.host } if params[:utm_medium].present?
-        cookies[:ref] = referer
+        cookies[:ref] = {:value => referer, :expires => COOKIE_EXPIRES,:domain => uri.host }
 
       end
 
