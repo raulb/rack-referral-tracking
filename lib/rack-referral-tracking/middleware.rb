4f275fdd2b44ceaa476cf44e49ddf82ffd404ead
Craig Kerstiens <craig.kerstiens@gmail.com>
2013-04-18 13:45:36 -0700

    initial stab at tracking relevant data

--- lib/rack-referral-tracking/middleware.rb
+++ lib/rack-referral-tracking/middleware.rb
@@ -1,3 +1,4 @@
+require 'uri'
 module Rack
   module ReferralTracking
     class Middleware
@@ -8,6 +9,16 @@ module Rack
 
       def call(env)
         @app.call(env)
+
+        referer = Fernet.generate(ENV['secret']) do |generator|
+          generator.data = { request.env["HTTP_REFERER"] }
+        end
+
+        cookies[:utm_campaign] = { :value => params[:utm_campaign], :expires => COOKIE_EXPIRES,:domain => uri.host } if params[:utm_campaign].present?
+        cookies[:utm_source] = { :value => params[:utm_source], :expires => COOKIE_EXPIRES,:domain => uri.host } if params[:utm_source].present?
+        cookies[:utm_medium] = { :value => params[:utm_medium], :expires => COOKIE_EXPIRES,:domain => uri.host } if params[:utm_medium].present?
+        cookies[:ref] = referer
+
       end
 
     end
