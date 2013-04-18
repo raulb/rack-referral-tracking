cc78d400d5b3497cdd5a194085a921ac5851a150
Matthew Conway <himself@mattonrails.com>
2013-04-18 16:19:44 -0700

    Fix invalid use of Hash#has_key?

--- lib/rack-referral-tracking/middleware.rb
+++ lib/rack-referral-tracking/middleware.rb
@@ -23,9 +23,9 @@ module Rack
           end
         end
 
-        Rack::Utils.set_cookie_header!(headers, "utm_campaign", :value => params['utm_campaign'], :domain => cookie_domain, :path => '/') if params.has_key? ['utm_campaign']
-        Rack::Utils.set_cookie_header!(headers, "utm_source", :value => params['utm_source'], :domain => cookie_domain, :path => '/') if params.has_key?['utm_source']
-        Rack::Utils.set_cookie_header!(headers, "utm_medium", :value => params['utm_medium'], :domain => cookie_domain, :path => '/') if params.has_key?['utm_medium']
+        Rack::Utils.set_cookie_header!(headers, "utm_campaign", :value => params['utm_campaign'], :domain => cookie_domain, :path => '/') if params.has_key? 'utm_campaign'
+        Rack::Utils.set_cookie_header!(headers, "utm_source", :value => params['utm_source'], :domain => cookie_domain, :path => '/') if params.has_key? 'utm_source'
+        Rack::Utils.set_cookie_header!(headers, "utm_medium", :value => params['utm_medium'], :domain => cookie_domain, :path => '/') if params.has_key? 'utm_medium'
         Rack::Utils.set_cookie_header!(headers, "ref", :value => referer, :domain => cookie_domain, :path => '/') if referer
 
         [status, headers, body]
